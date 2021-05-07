38cba4f7ec6776aad12d2f91f286f3ee0cfffef6
yrogovich <yRogovich@gmail.com>
2021-05-07 18:44:53 +0300

    Release v1.1.1

--- src/js/googleMap.js
+++ src/js/googleMap.js
@@ -5,7 +5,7 @@ export async function mapLoad(api) {
 	
 	const google = await loader.load();
 
-	const image = `${window.location.protocol}//${window.location.hostname}/img/icons/map-pin.png`
+	const image = 'https://raw.githubusercontent.com/yrogovich/craftonTask/150dcb6732ba0336908740253810a4508a70ecb7/img/icons/map-pin.png'
 	const point = { lat: 52.40315998103853, lng: 16.90997132280444 }
 	const map = new google.maps.Map(document.getElementById('map'), {
 		center: point,
